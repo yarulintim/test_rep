fc63cac9da0823cbc00437f5dfdc1b217c92734a
Timur Yarulin <yarulintim@gmail.com>
2018-04-14 15:28:45 +0300

    add * py

--- showdoc.py
+++ showdoc.py
@@ -1,4 +1,4 @@
 import time
-print('hi')
+print('hi*')
 print('> > $$ Gerrit is a web-based code review tool built on top of the Git version control system. This introduction provides an overview of Gerrit and describes how Gerrit integrates into a typical development workflow. It also provides a brief tutorial that shows how to manage a change using Gerrit.')
 time.sleep(1.2)
